59b07ffea4024fcccf23271491a053cfb041f128
Jonathan East <jonathan_east@ymail.com>
2020-02-26 19:14:25 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -1,6 +1,6 @@
 function forLoop(array) {
   for (var i = 0; i < 25; i++) {
-      array = `"I am ${array[i]} strange loop${array[i] === 0 ? '' : 's'}."`;
+      array.push(`"I am ${array[i]} strange loop${array[i] === 0 ? '' : 's'}."`);
   }
   return array;
 }
