13bca052192c596e916ad1dde33546d8b071cbce
Jonathan East <jonathan_east@ymail.com>
2020-02-26 19:23:02 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -4,3 +4,13 @@ function forLoop(array) {
   }
   return array;
 }
+
+function whileLoop(n) {
+  while(n > 0) {
+    console.log(n);
+    n--;
+    if(n === 0) {
+      console.log("done");
+    }
+  }
+}
