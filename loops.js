4b6ff38684c33a71ca06832ca55c76b4e68186cf
Jonathan East <jonathan_east@ymail.com>
2020-02-26 19:34:13 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -18,12 +18,12 @@ function whileLoop(n) {
 function doWhileLoop(num) {
   var i = 0;
 
-function incrementVariable() {
-  i = i + 1;
-  return i;
-}
+    function incrementVariable() {
+      i = i + 1;
+      return i;
+    }
 
-do {
-  console.log("I run once regardless.");
-} while (incrementVariable() < num);
+    do {
+      console.log("I run once regardless.");
+    } while (incrementVariable() < num);
 }
