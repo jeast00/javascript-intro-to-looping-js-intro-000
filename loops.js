c7e9dbbe1a53eb232106e03c2bbb7b004a05bd13
Jonathan East <jonathan_east@ymail.com>
2020-02-26 19:33:56 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -14,3 +14,16 @@ function whileLoop(n) {
     }
   }
 }
+
+function doWhileLoop(num) {
+  var i = 0;
+
+function incrementVariable() {
+  i = i + 1;
+  return i;
+}
+
+do {
+  console.log("I run once regardless.");
+} while (incrementVariable() < num);
+}
